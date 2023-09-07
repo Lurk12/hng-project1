03ac0eba3e2b56ba0a5e2d36cc5f2f46d5838611
Nifemi <aborisadenifemi@gmail.com>
2023-09-07 22:01:07 +0100

    hng first task

--- app.js
+++ app.js
@@ -1,40 +1,10 @@
 const express = require('express');
 const app = express();
-const {StatusCodes} = require('http-status-codes')
 
-const {createResponse} = require('./response')
+const mainRouter = require('./controllers/main')
 
-app.get('/api', (req, res) => {
-    const defaultSlackName = 'Lurk';
-    const defaultTrack = 'Backend'; 
+app.use('/', mainRouter);
 
-    const slack_name = req.query.slackName || defaultSlackName;
-    const track = req.query.track || defaultTrack;
-try {
-    const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    const currentDay = daysOfWeek[new Date().getDay()];
-    // console.log(currentDay);
-
-    const currentUtcTime = new Date();
-    currentUtcTime.setMinutes(currentUtcTime.getMinutes() - 2); // Subtract 2 minutes for validation
-    const currentTime = currentUtcTime.toISOString();
-    // console.log(currentTime);
-
-    
-    const githubFileUrl = 'https://github.com/Lurk12/hng-project1'
-    const githubSourceUrl = 'https://github.com/Lurk12/hng-project1.git'
-    
-
-    const response = createResponse(slack_name, currentDay, currentTime, track, githubFileUrl, githubSourceUrl);
-
-    res.status(StatusCodes.OK).json(response);
-    console.log(response);
-
-} catch (error) {
-    console.log(error);
-}
-  
-})
 const port = process.env.PORT || 2000;
 
 app.listen(port, ()=>{
---
+++ controllers/main.js
@@ -0,0 +1,33 @@
+const router = require('express').Router();
+const { StatusCodes } = require('http-status-codes');
+const { createResponse } = require('../response');
+
+router.get('/api', (req, res) => {
+  const defaultSlackName = 'Lurk';
+  const defaultTrack = 'Backend';
+
+  const slackName = req.query.slackName || defaultSlackName;
+  const track = req.query.track || defaultTrack;
+
+  try {
+    const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const currentDay = daysOfWeek[new Date().getDay()];
+
+    const currentUtcTime = new Date();
+    currentUtcTime.setMinutes(currentUtcTime.getMinutes() - 2); // Subtract 2 minutes for validation
+    const currentTime = currentUtcTime.toISOString();
+
+    const githubFileUrl = 'https://github.com/Lurk12/hng-project1';
+    const githubSourceUrl = 'https://github.com/Lurk12/hng-project1.git';
+
+    const response = createResponse(slackName, currentDay, currentTime, track, githubFileUrl, githubSourceUrl);
+
+    res.status(StatusCodes.OK).json(response);
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: 'Internal Server Error' });
+  }
+});
+
+module.exports = router;
