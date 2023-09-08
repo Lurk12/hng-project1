9cfa3c0cbb6ad5818ad4009a9eb6a230a469c145
Nifemi <aborisadenifemi@gmail.com>
2023-09-08 11:53:22 +0100

    hng first task

--- response.js
+++ response.js
@@ -1,15 +1,14 @@
-
 const { StatusCodes } = require("http-status-codes");
 
 function createResponse(slack_name, currentDay, currentTime, track, githubFileUrl, githubSourceUrl) {
     return {
-        "Slack Name": slack_name,
-        "Current Day of the Week": currentDay,
-        "Current UTC Time": currentTime,
+        "slack_name": slack_name,
+        "current_day": currentDay,
+        "utc_time": currentTime,
         "track": track,
-        "Github File URL": githubFileUrl,
-        "Github Repo URL": githubSourceUrl,
-        "Status Code": StatusCodes.OK
+        "github_file_url": githubFileUrl,
+        "github_repo_url": githubSourceUrl,
+        "status_code": StatusCodes.OK 
     };
 }
 
